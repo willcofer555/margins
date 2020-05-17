78b5114a3781eb50a0282d3405567b4022ae9e45
William Cofer <willcofer@gmail.com>
2020-05-17 18:06:05 -0500

    root config

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,1 @@
-import express from 'express';
-
-const app = express();
-const port = 3000;
-app.get('/', (req, res) => {
-    res.send('Testing node TypeScript app');
-  });
-  app.listen(port, err => {
-    if (err) {
-      return console.error(err);
-    }
-    return console.log(`server is listening on ${port}`);
-  });
+import * as React from "react";
